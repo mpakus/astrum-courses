8dcfbc27eff80dd028dd3b6f72a610c7ae021484
MpaK <mrak69@gmail.com>
2026-01-19 14:24:08 -0600

    Feature: deployment, colors and grades of 2 added

--- react2/vite-project/src/App.jsx
+++ react2/vite-project/src/App.jsx
@@ -5,6 +5,17 @@ import "./App.css";
 import UserCard from "./Components/UserCard";
 import picUrl from "./assets/pic-1.png";
 
+const grades = {
+  1: { name: "2^0", color: "#FF0000" },
+  2: { name: "2^1", color: "#FF7F00" },
+  4: { name: "2^2", color: "#FFFF00" },
+  8: { name: "2^3", color: "#00FF00" },
+  16: { name: "2^4", color: "#00FFFF" },
+  32: { name: "2^5", color: "#0000FF" },
+  64: { name: "2^6", color: "#8B00FF" },
+  128: { name: "2^7", color: "#FF00FF" },
+};
+
 function App() {
   let [numbers, setNumbers] = useState([]);
   let [bytes, setBytes] = useState([128, 64, 32, 16, 8, 4, 2, 1]);
@@ -42,14 +53,17 @@ function App() {
         }}
       >
         <ul style={{ listStyleType: "none", padding: 0 }}>
-          {bytes.map((byte) => (
+          {bytes.map((byte, index) => (
             <li key={`byte_${byte}`} className="check">
               <input
                 type="checkbox"
                 id={`byte_${byte}`}
                 onChange={() => checkNumber({ num: byte })}
               />
-              <label htmlFor={`byte_${byte}`}>{byte}</label>
+              <label htmlFor={`byte_${byte}`}>
+                <strong style={{ color: grades[byte]["color"] }}>{byte}</strong>{" "}
+                <small>({grades[byte]["name"]})</small>
+              </label>
             </li>
           ))}
         </ul>
